6535796a3d01cdebb3e38384b6fada4262fc4e20
Leon <student@epicodus.com>
2015-04-24 10:41:08 -0700

    Input happy to see how many times happy is in the string, test pass.

--- lib/code_review.rb
+++ lib/code_review.rb
@@ -1,5 +1,13 @@
 class String
   define_method(:string_count) do
-    "a".count("a")
-  end
+#    "a".count("a")
+      def count_frequency(word_list)
+        counts = Hash.new(0)
+       for word in word_list
+      counts[word] += 1
+     end
+    counts
+   end
+  p count_frequency(["happy", "happy"])
+ end
 end
--- spec/code_review_spec.rb
+++ spec/code_review_spec.rb
@@ -5,4 +5,8 @@ describe('String#string_count') do
   it('Input a to see how many times a apears in a') do
     expect("a".string_count()).to(eq(1))
   end
+
+  it('Input happy to see how many times happy apears in a string') do
+    expect("happy".string_count()).to(eq({"happy"=>2}))
+  end
 end
